e00166cbae5a9cc2de220b9cd182fa1b029c90cd
Fergus Barker <fergusbarker@gmail.com>
2012-02-23 23:36:41 +1100

    Initial repo made! Fill 'er up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitClone::Application.config.secret_token = '2a1ca13761e3e94a3c4b07e5d70645cd417f56c06b5455f6011a3f21522d6cde3c7b7b9edd0e8f712157e4ba9ef6a5042c92b231bbd362a1a99a6446df9f2f32'
