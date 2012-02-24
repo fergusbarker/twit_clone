b131e4a511c250ab88d49352d4a8f17550557010
Fergus Barker <fergusbarker@gmail.com>
2012-02-24 21:37:49 +1100

    Added settings to do tests through spork. A wise choice, but required shifting around for rails V3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,15 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+# spork as testing server
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  unless defined?(Rails)
+    require File.dirname(__FILE__) + "/../config/environment"
+  end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -29,4 +38,10 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  ActiveSupport::Dependencies.clear
+end
+end
+
+Spork.each_run do
 end
