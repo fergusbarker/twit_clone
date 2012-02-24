044a6bbe5232cffb004597759cd4596bb2ea7ce4
Fergus Barker <fergusbarker@gmail.com>
2012-02-24 22:13:32 +1100

    Added the about page. Dynamicise it soon lad

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -12,6 +14,13 @@ describe PagesController do
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
       response.should be_success
     end
   end
