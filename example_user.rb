2c8f0dfe3701b3d077ff998d297813d600ff05b5
Fergus Barker <fergusbarker@gmail.com>
2012-03-05 17:18:28 +1100

    Adding a usercreation script

---
+++ example_user.rb
@@ -0,0 +1,14 @@
+# For upcoming class structures
+
+class User
+  attr_accessor :user, :email
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+end
