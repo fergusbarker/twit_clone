8e9f27856a4ccf03dec7ad03035ddc32cf36e3aa
Fergus Barker <fergusbarker@gmail.com>
2012-03-20 20:40:27 +1100

    Adding in testing for name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = {:name => "Example User", :email => "user@example.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_user_name = User.new(@attr.merge(:name => ""))
+    no_user_name.should_not be_valid
+  end
+
 end
