abb929e281ed4b5ebade93045e09cbdf684b3c41
Fergus Barker <fergusbarker@gmail.com>
2012-03-20 20:32:36 +1100

    Commenting out for user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+# Schema version: <timestamp>
+#
+# Table name: users
+#
+# id :integer
+# name :string(255)
+# email :string(255)
+# created_at :datetime
+# updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+#  validates :name, :presence => true
 end
