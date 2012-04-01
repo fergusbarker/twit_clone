d54170eb021fbe22deefe84c8325912f2d839d6c
Fergus Barker <fergusbarker@gmail.com>
2012-04-02 09:13:03 +1000

    Added testing for invalid email addresses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,4 +26,22 @@ describe User do
     too_long_user.should_not be_valid
   end
 
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org example.user@foo.biz]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+
+
+
 end
